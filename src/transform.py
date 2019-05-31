9f04bb0b48f0e4c0618818e6af0ef8d881352b29
Mark Frost <cheerful.mf@gmail.com>
2019-06-01 01:36:56 +0300

    made transform (lesson 1)

---
+++ src/transform.py
@@ -0,0 +1,24 @@
+import torch
+from torch import utils
+from torchvision import datasets, transforms
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+path='./MNIST_data'
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0,), (1,)),
+])
+
+def mnist(batch_size=10, shuffle=False, transform=transform, path='./MNIST_data'):
+    train_data = datasets.MNIST(path, train=True, download=True, transform=transform)
+    train_loader = utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
+
+    return train_loader
+
+
+mnist()
