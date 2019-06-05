20995337dd0d1649c182311710b4beb25662f1a2
Mark Frost <cheerful.mf@gmail.com>
2019-06-05 21:16:10 +0500

    fix

--- src/transform.py
+++ src/transform.py
@@ -2,7 +2,8 @@ import torch
 from torch import utils
 from torchvision import datasets, transforms
 
-path='./MNIST_data'
+path = './MNIST_data'
+
 
 class CustomTransform():
     def __init__(self, base_mean, base_std):
@@ -11,31 +12,38 @@ class CustomTransform():
         pass
 
     def __call__(self, tensor):
-        mean = torch.mean(tensor)
-        std = torch.std(tensor)
-
         return (tensor - self.base_mean) / self.base_std
 
 
-def mnist(batch_size=10, shuffle=False, path='./MNIST_data'):
-    base_data = datasets.MNIST(path, train=True, download=True, transform=transforms.ToTensor())
+def mnist(batch_size=20, shuffle=False, path='./MNIST_data'):
+    base_data = datasets.MNIST(
+        path, train=True, download=True, transform=transforms.ToTensor())
+
+    base_data_loader = utils.data.DataLoader(base_data, batch_size=60000)
+    base_list = next(iter(base_data_loader))
+
+    base_mean = torch.mean(base_list[0].flatten()).item()
+    base_std = torch.std(base_list[0].flatten()).item()
 
     transform = transforms.Compose([
         transforms.ToTensor(),
-        CustomTransform(torch.mean(base_data[0][0]), torch.std(base_data[0][0])),
+        CustomTransform(base_mean, base_std),
     ])
 
-    train_data = datasets.MNIST(path, train=True, download=True, transform=transform)
-    train_loader = utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
+    train_data = datasets.MNIST(
+        path, train=True, download=True, transform=transform)
+    train_loader = utils.data.DataLoader(
+        train_data, batch_size=batch_size, shuffle=shuffle)
 
     print("Tensor mean: %f" % torch.mean(train_data[0][0]))
     print("Tensor std: %f" % torch.std(train_data[0][0]))
 
     return train_loader
 
+
 mnist()
 
 
 # RESULT:
-#  Tensor mean: 0.000000
-#  Tensor std: 1.000000
+# Tensor mean: 0.022777
+# Tensor std: 1.014410
